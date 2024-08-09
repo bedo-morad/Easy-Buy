d615b2fe36158a84293a3250fc33bb2dd93ca75f
bedo <bodymosad67@gmail.com>
2024-08-09 06:29:13 +0300

    🎉 init project ✨ Config Server and Discovery

---
+++ services/discovery/src/test/java/com/easybuy/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybuy.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
